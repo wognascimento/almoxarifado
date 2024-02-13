2e79380922aca4a4111e8fc5766102cca8436960
wesley <wog.nascimento@gmail.com>
2024-02-13 16:57:03 -0300

    Adicionar arquivos de projeto.

---
+++ Almoxarifado/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Almoxarifado.DataBase;
+using System.Windows;
+
+namespace Almoxarifado
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Add your Syncfusion license key for WPF platform with corresponding Syncfusion NuGet version referred in project. For more information about license key see https://help.syncfusion.com/common/essential-studio/licensing/license-key.
+            // Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your license key here"); 
+
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzI0OUAzMjMzMkUzMTJFMzljWE9mZml0ZnFGQ2xaaUFSUXBGeVdweDhiOCtBbUlDSWptZEgyVTYrcU1ZPQ==");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjM5MkAzMjM0MkUzMTJFMzlZRnNmeEdKa0haRGU0S0MyZUR3b05vcDJFNURBbnFRTi9STUVidExydWswPQ==");
+
+            DataBaseSettings BaseSettings = DataBaseSettings.Instance;
+            BaseSettings.Database = DateTime.Now.Year.ToString();
+            BaseSettings.Host = "postgresql-server";
+            BaseSettings.Username = Environment.UserName;
+            BaseSettings.Password = "123mudar";
+
+        }
+    }
+
+}
---
+++ Almoxarifado/DataBase/DataBaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Almoxarifado.DataBase
+{
+    public sealed class DataBaseSettings
+    {
+        private static readonly DataBaseSettings instance = new();
+        public string? Host { get; set; }
+        public string? Database { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public static DataBaseSettings Instance => instance;
+    }
+}
