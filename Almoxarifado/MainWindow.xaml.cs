3d84458d311dea9ca1ab8c150172abdf87886a4a
wesley <wog.nascimento@gmail.com>
2024-02-13 18:38:15 -0300

    TELAS E TRADUÇÃO DE COMPONENTES

--- Almoxarifado/MainWindow.xaml.cs
+++ Almoxarifado/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Almoxarifado.DataBase;
+using Almoxarifado.Views.Movimentacoes;
 using Syncfusion.SfSkinManager;
+using Syncfusion.Windows.Tools.Controls;
 using System.Collections.Specialized;
 using System.Configuration;
 using System.Text;
@@ -74,9 +76,9 @@ namespace Almoxarifado
         /// <remarks></remarks>
         private void OnSizeModeChanged()
         {
-            SizeMode sizeMode = SizeMode.Default;
+            Syncfusion.SfSkinManager.SizeMode sizeMode = Syncfusion.SfSkinManager.SizeMode.Default;
             Enum.TryParse(CurrentSizeMode, out sizeMode);
-            if (sizeMode != SizeMode.Default)
+            if (sizeMode != Syncfusion.SfSkinManager.SizeMode.Default)
             {
                 SfSkinManager.ApplyStylesOnApplication = true;
                 SfSkinManager.SetSizeMode(this, sizeMode);
@@ -113,22 +115,102 @@ namespace Almoxarifado
 
         private void _mdi_CloseAllTabs(object sender, Syncfusion.Windows.Tools.Controls.CloseTabEventArgs e)
         {
-
+            _mdi.Items.Clear();
         }
 
         private void _mdi_CloseButtonClick(object sender, Syncfusion.Windows.Tools.Controls.CloseButtonEventArgs e)
         {
+            var tab = (DocumentContainer)sender;
+            _mdi.Items.Remove(tab.ActiveDocument);
+        }
 
+        private FrameworkElement ExistDocumentInDocumentContainer(string name_)
+        {
+            foreach (FrameworkElement element in _mdi.Items)
+            {
+                if (name_.ToLower() == element.Name)
+                {
+                    return element;
+                }
+            }
+            return null;
+        }
+
+        public void adicionarFilho(object filho, string title, string name)
+        {
+            var doc = ExistDocumentInDocumentContainer(name);
+            if (doc == null)
+            {
+                doc = (FrameworkElement?)filho;
+                DocumentContainer.SetHeader(doc, title);
+                doc.Name = name.ToLower();
+                _mdi.Items.Add(doc);
+            }
+            else
+            {
+                //_mdi.RestoreDocument(doc as UIElement);
+                _mdi.ActiveDocument = doc;
+            }
         }
 
         private void OnAlterarUsuario(object sender, MouseButtonEventArgs e)
         {
+            Login window = new();
+            window.ShowDialog();
 
+            try
+            {
+                var appSettings = ConfigurationManager.GetSection("appSettings") as NameValueCollection;
+
+                if (appSettings[0] == null || appSettings[0] == "")
+                {
+                    return;
+                }
+                BaseSettings.Username = appSettings[0];
+                txtUsername.Text = BaseSettings.Username;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void OnAlterarAno(object sender, MouseButtonEventArgs e)
         {
+            RadWindow.Prompt(new DialogParameters()
+            {
+                Header = "Ano Sistema",
+                Content = "Alterar o Ano do Sistema",
+                Closed = (object sender, WindowClosedEventArgs e) =>
+                {
+                    if (e.PromptResult != null)
+                    {
+                        BaseSettings.Database = e.PromptResult;
+                        txtDataBase.Text = BaseSettings.Database;
+                        _mdi.Items.Clear();
+                    }
+                }
+            });
+        }
 
+        private void OnAlmoxSaidaClick(object sender, RoutedEventArgs e)
+        {
+            adicionarFilho(new AlmoxSaida(), "ALMOXARIFADO MOVIMENTAÇÃO DE SAÍDA", "ALMOX_MOV_SAIDA");
+        }
+
+        private void OnAlmoxEntradaClick(object sender, RoutedEventArgs e)
+        {
+            adicionarFilho(new AlmoxEntrada(), "ALMOXARIFADO MOVIMENTAÇÃO DE ENTRADA", "ALMOX_MOV_ENTRADA");
+        }
+
+        private void OnBolsaSaidaClick(object sender, RoutedEventArgs e)
+        {
+            adicionarFilho(new BolsaSaida(), "ALMOXARIFADO BOLSA MOVIMENTAÇÃO DE SAÍDA", "ALMOX_BOLSA_MOV_SAIDA");
+        }
+
+        private void OnBolsaEntradaClick(object sender, RoutedEventArgs e)
+        {
+            adicionarFilho(new BolsaEntrada(), "ALMOXARIFADO BOLSA MOVIMENTAÇÃO DE ENTRADA", "ALMOX_BOLSA_MOV_ENTRADA");
         }
     }
 }
