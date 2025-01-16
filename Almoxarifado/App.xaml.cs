f488345d18213be9307fd6bb1683800e8f61217f
wesley <wog.nascimento@gmail.com>
2025-01-16 12:40:10 -0300

    ATUALIZAÇÃO BIBLIOTECA

--- Almoxarifado/App.xaml.cs
+++ Almoxarifado/App.xaml.cs
@@ -15,8 +15,9 @@ namespace Almoxarifado
             // Add your Syncfusion license key for WPF platform with corresponding Syncfusion NuGet version referred in project. For more information about license key see https://help.syncfusion.com/common/essential-studio/licensing/license-key.
             // Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Add your license key here"); 
 
-            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzI0OUAzMjMzMkUzMTJFMzljWE9mZml0ZnFGQ2xaaUFSUXBGeVdweDhiOCtBbUlDSWptZEgyVTYrcU1ZPQ==");
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjM5MkAzMjM0MkUzMTJFMzlZRnNmeEdKa0haRGU0S0MyZUR3b05vcDJFNURBbnFRTi9STUVidExydWswPQ==");
+            
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjM5MkAzMjM0MkUzMTJFMzlZRnNmeEdKa0haRGU0S0MyZUR3b05vcDJFNURBbnFRTi9STUVidExydWswPQ==");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTU4NUAzMjM3MkUzMTJFMzluT08wbzRnYm4zUlFDOVRzWVpYbUtuSEl0aUhTZmNMYjQxekhrV0NVRnlzPQ==");
 
             DataBaseSettings BaseSettings = DataBaseSettings.Instance;
             BaseSettings.Database = DateTime.Now.Year.ToString();
