b0dfaadcf40ca31c2c5eccbf7c2fb7b1bf23852b
wesley <wog.nascimento@gmail.com>
2025-03-31 19:21:18 -0300

    APLICADO ATUALIZAÇÃO

--- Almoxarifado/App.xaml.cs
+++ Almoxarifado/App.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using Almoxarifado.DataBase;
 using Almoxarifado.Localization;
+using BibliotecasSIG;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
 using System.Windows;
 using Telerik.Windows.Controls;
 
@@ -10,6 +15,9 @@ namespace Almoxarifado
     /// </summary>
     public partial class App : Application
     {
+        private const string UPDATE_URL = "http://192.168.0.49/downloads/almoxarifado/version.json";
+        private readonly string CURRENT_VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
         public App()
         {
             // Add your Syncfusion license key for WPF platform with corresponding Syncfusion NuGet version referred in project. For more information about license key see https://help.syncfusion.com/common/essential-studio/licensing/license-key.
@@ -21,7 +29,7 @@ namespace Almoxarifado
 
             DataBaseSettings BaseSettings = DataBaseSettings.Instance;
             BaseSettings.Database = DateTime.Now.Year.ToString();
-            BaseSettings.Host = "postgresql-server";
+            BaseSettings.Host = "192.168.0.23";
             BaseSettings.Username = Environment.UserName;
             BaseSettings.Password = "123mudar";
 
@@ -31,6 +39,77 @@ namespace Almoxarifado
             };
 
         }
-    }
 
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Verificação de atualização em segundo plano
+            await CheckForUpdatesAsync();
+        }
+
+
+        private async Task CheckForUpdatesAsync()
+        {
+            try
+            {
+                var updateChecker = new UpdateChecker(UPDATE_URL, CURRENT_VERSION);
+                var updateInfo = await updateChecker.CheckForUpdatesAsync();
+
+                var updateInfoJson = JsonSerializer.Serialize<UpdateInfo>(updateInfo);
+
+                if (updateInfo != null)
+                {
+                    // Pergunta ao usuário se deseja atualizar
+                    var result = MessageBox.Show(
+                        $"Nova versão disponível!\n\n" +
+                        $"Versão atual: {CURRENT_VERSION}\n" +
+                        $"Nova versão: {updateInfo.updateVersion}\n\n" +
+                        "Changelog:\n" +
+                        string.Join("\n", updateInfo.changelog) +
+                        "\n\nDeseja baixar a atualização?",
+                        "Atualização Disponível",
+                        MessageBoxButton.YesNo,
+                        MessageBoxImage.Information
+                    );
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+
+                        var options = new JsonSerializerOptions { WriteIndented = true };
+                        string jsonString = JsonSerializer.Serialize(updateInfo, options);
+
+                        //Process.Start("Update.exe", @$"{updateInfoJson}, Almoxarifado.exe");
+
+                        string jsonData = JsonSerializer.Serialize(updateInfo); // Garante que o JSON está bem formatado
+                        string appName = "Almoxarifado.exe";
+
+                        string arguments = $"\"{jsonData.Replace("\"", "\\\"")}\" \"{appName}\"";
+                        Process.Start("Update.exe", arguments);
+                        this.Shutdown();
+
+                    }
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                // Log do erro ou tratamento de exceção
+                MessageBox.Show(
+                    $"Erro ao verificar atualizações: {ex.Message}",
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Erro ao verificar atualizações: {ex.Message}",
+                    "Erro",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+            }
+        }
+    }
 }
